f4a3dd7566f06a8db24c640bbda03972ebb228ae
Umar Nazarov <1nazarovumar@gmail.com>
2022-03-31 18:42:03 +0500

    imageIntro

--- src/components/Header.client.jsx
+++ src/components/Header.client.jsx
@@ -35,7 +35,6 @@ export default function Header({ collections, storeName, fromHome }) {
         }
     }, [])
 
-
   useEffect(() => {
     const scrollbarWidth = document.documentElement.clientWidth;
 
---
+++ src/components/ImageIntro.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Men from '../../images/Men.webp'
+
+function ImageIntro() {
+  return (
+      <div className='relevant'>
+          <img className='h-[110vh] w-[100%] object-cover'width="100%" height='100vh' src={Men} />
+          <div className='absolute bottom-[150px] right-0 left-0 text-center text-white font-bold '>
+                <span className='text-1xl tracking-wider'>CASUAL TO CLASSIC</span>
+                <h1 className='text-5xl tracking-wide sm:text-3xl'>LUXURY TRAVELWEAR</h1>
+                <button className='bg-white text-black text-base py-[10px] px-[20px] mt-6'>SHOP NOW</button>
+            </div>
+    </div>
+  )
+}
+
+export default ImageIntro
